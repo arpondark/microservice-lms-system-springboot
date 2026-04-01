2c6c9f6565e744a42a1fec681af0b740bdc1ee39
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-04-01 23:01:15 +0600

    user service init

---
+++ USER-SERRVICE/src/test/java/site/shazan/USER_SERRVICE/UserSerrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shazan.USER_SERRVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSerrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
