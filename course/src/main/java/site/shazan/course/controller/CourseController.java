81010e656d5d9fe34404d45bc6b065e404412f96
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-04-09 14:44:04 +0600

    course-service added but controller is not configured

---
+++ course/src/main/java/site/shazan/course/controller/CourseController.java
@@ -0,0 +1,11 @@
+package site.shazan.course.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+@RequiredArgsConstructor
+public class CourseController {
+}
---
+++ course/src/main/java/site/shazan/course/models/Course.java
@@ -0,0 +1,25 @@
+package site.shazan.course.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String courseName;
+    private String courseCode;
+    private String courseDescription;
+    private String imageUrl;
+    private String teacherName;
+    private String videoUrl;
+    private String courseMaterialUrl;
+    private String comment;
+}
